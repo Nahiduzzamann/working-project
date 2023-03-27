dfdb0104083d0ce9035ae081b34860beeff6cef8
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-03-27 12:59:55 +0600

    added header

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './Header.css';
+import logo from '../../asset/images/Logo.svg'
+
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header111</h1>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="logo" />
+            <div>
+                <a href="/shop">shop</a>
+                <a href="/order">order</a>
+                <a href="/inventory">inventory</a>
+                <a href="/login">login</a>
+            </div>
+        </nav>
     );
 };
 
