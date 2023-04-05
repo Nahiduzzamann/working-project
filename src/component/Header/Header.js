d6371136de0238d0b5b947884723bd265b9290bc
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-04-05 13:54:44 +0100

    route updated

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="logo" />
             <div>
-                <Link to={"/shop"}>Shop</Link>
+                <Link to={"/"}>Shop</Link>
                 <Link to={"/orders"}>Orders</Link>
                 <Link to={"/inventory"}>Inventory</Link>
                 <Link to={"/login"}>Login</Link>       
